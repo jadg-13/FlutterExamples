ca269fb8384ab07362ae739d76cca72d7f72c9a5
jadg1308 <jadg13@hotmail.com>
2023-10-28 21:46:50 -0600

    [update] list view

--- listview2/lib/main.dart
+++ listview2/lib/main.dart
@@ -12,61 +12,71 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage  extends StatelessWidget{
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
 
-  List<String> items = List<String>.generate(20, (index) => "https://picsum.photos/250?image=$index");
+  List<String> items = List<String>.generate(
+      20, (index) => "https://picsum.photos/250?image=$index");
+
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     appBar: AppBar(
-       title: Text("List View 2"),
-     ),
-     body: ListView.builder(
-       itemCount: items.length,
-       itemBuilder: (BuildContext context, int index){
-         final img = items[index];
-         return ListTile(
-           title: Text("Imagen ${index+1}"),
-           subtitle: Text("Subtitle $index"),
-           leading: Image.network(img),
-           trailing: IconButton(
-             icon: Icon(Icons.remove_red_eye_sharp),
-             onPressed: (){
-               alertDialog(context, img);
-             },
-           )
-         );
-       },
-     )
-
-   );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("List View 2"),
+      ),
+      body: Column(
+        children: [
+          Text(
+            "Lista de imagenes",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          Image.asset(
+            "assets/logo.png",
+            height: 200,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (BuildContext context, int index) {
+                final img = items[index];
+                return ListTile(
+                    title: Text("Imagen ${index + 1}"),
+                    subtitle: Text("Subtitle $img"),
+                    leading: Image.network(img),
+                    trailing: IconButton(
+                      icon: Icon(Icons.remove_red_eye_sharp),
+                      onPressed: () {
+                        alertDialog(context, img);
+                      },
+                    ));
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   void alertDialog(BuildContext context, String img) {
     showDialog(
         context: context,
-        builder: (BuildContext context){
+        builder: (BuildContext context) {
           return AlertDialog(
             title: Text("Imagen"),
             content: Image.network(img),
             actions: [
               TextButton(
-                  onPressed: (){
+                  onPressed: () {
                     Navigator.of(context).pop();
                   },
-                  child: Text("Cerrar")
-              )
+                  child: Text("Cerrar"))
             ],
           );
-        }
-    );
+        });
   }
-
 }
